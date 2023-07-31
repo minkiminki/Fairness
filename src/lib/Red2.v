2773e821ff081c0f10572b71c51b610260d01049
Minki Cho <minki.cho@sf.snu.ac.kr>
2023-07-31 12:58:06 +0900

    WIP

--- src/lib/Red2.v
+++ src/lib/Red2.v
@@ -1,32 +1,468 @@
+Require Import String.
 From Fairness Require Import MyLtac2.
 
 Ltac2 Type flag := [ Break | Continue | Fail ].
 
-(* Internals *)
-Lemma _equal_f (A B : Type) (f g : A -> B)
-      x
-      (EQ: f = g)
-  :
-    f x = g x.
-Proof.
-  subst. apply (@eq_refl).
-Qed.
+Lemma eq_rw (X: Type) (P: X -> Type) (x: X) (y: X) (EQ: x = y): P y -> P x.
+Proof. exact (fun H => @eq_rect_r _ _ P H _ EQ). Qed.
 
-Lemma _einit (P Q: Prop)
-      (EQ: P = Q)
-  :
-    Q -> P.
-Proof.
-  subst. apply id.
-Qed.
+Ltac2 ctx_rw p :=
+  let x := open_constr:(_) in
+  let c := Std.eval_hnf open_constr:($p $x) in
+  Std.unify c (Control.goal ());
+  refine (open_constr:(@eq_rw _ $p $x _ _ _)) >
+    [Control.shelve ()| |hnf].
 
-Ltac2 rec _ctx n :=
-  if (Int.le n 0)
-  then
-    (apply f_equal)
-  else
-    (apply _equal_f; _ctx (Int.sub n 1))
+
+Variant red_class: Type := | red_class_cons: string -> red_class.
+
+Class red_db (c: red_class)
+      (A: Type)
+      (a: A) :=
+  mk_red_db {
+      red_lemma_type: Type;
+      red_lemma: red_lemma_type;
+      red_focused_type: Type;
+      red_focused: red_focused_type;
+      red_next: (unit + red_class)%type;
+    }.
+Arguments red_db _ [_] _.
+Arguments mk_red_db _ [_] _ [_] _ [_] _ _.
+Arguments red_lemma [_ _ _] _.
+Arguments red_focused [_ _ _] _.
+Arguments red_next [_ _ _] _.
+
+Class red_db_incl (c0: red_class) :=
+  mk_red_db_incl { red_db_incl_next: red_class; }.
+Arguments red_db_incl_next [_] _.
+
+Ltac2 rec _red_tac c term k :=
+  match c with
+  | Some c =>
+      or (fun _ =>
+            let tc := tcsearch constr:(@red_db $c _ $term) in
+            let lem := (Std.eval_red (Std.eval_red constr:(red_lemma $tc))) in
+            let focused := (Std.eval_red (Std.eval_red constr:(red_focused $tc))) in
+            let next := (Std.eval_red (Std.eval_red constr:(red_next $tc))) in
+            let next := match! next with
+                        | inr ?next => Some constr:($next)
+                        | inl _ => None
+                        end in
+            _red_tac next focused (fun _ => k; eapply $lem))
+         (fun _ =>
+            let tc := tcsearch constr:(@red_db_incl $c) in
+            let next := (Std.eval_red (Std.eval_red constr:(red_db_incl_next $tc))) in
+            _red_tac (Some next) term k)
+  | None =>
+      k ()
+  end.
+
+Ltac2 red_tac c :=
+  match! goal with
+  | [ |- ?term = _ ] =>
+      (_red_tac (Some c) term (fun _ => ()))
+  end
 .
+
+Module TUTORIAL.
+  Section FOO.
+    (* Variables *)
+    Variable A B C: Type.
+    Variable a b c d: A.
+    Variable x y z: B.
+    Variable p q: C.
+    Variable f: B -> B.
+
+    Variable cl_C: red_class.
+    Variable cl_B: red_class.
+    Variable cl_B_unfold: red_class.
+    Variable cl_A: red_class.
+
+    Variable sim: A -> (nat * B) * C -> nat -> Prop.
+
+    (* First Step: Prove Reduction Lemmas *)
+    Hypothesis foo_red0: a = b.
+    Hypothesis foo_red1: b = c.
+    Hypothesis foo_red2: c = d.
+    Hypothesis foo_red3: x = y.
+    Hypothesis foo_red4: y = z.
+    Hypothesis foo_red5: p = q.
+
+
+    Instance foo_red1_hint: red_db cl_A a :=
+      mk_red_db _ _ foo_red0 b (inl tt).
+
+    Instance foo_red2_hint: red_db cl_A b :=
+      mk_red_db _ _ foo_red1 c (inl tt).
+
+    Instance foo_red3_hint: red_db cl_B_unfold x :=
+      mk_red_db _ _ foo_red3 y (inl tt).
+
+    Instance foo_red4_hint: red_db cl_B_unfold y :=
+      mk_red_db _ _ foo_red4 y (inl tt).
+
+    Instance foo_red5_hint: red_db cl_C p :=
+      mk_red_db _ _ foo_red5 q (inl tt).
+
+    Instance cl_B_unfold_cl_B: red_db_incl cl_B := mk_red_db_incl _ cl_B_unfold.
+
+    (* Instance cl_B_unfold_cl_B: red_db_incl cl_B_unfold cl_B := mk_red_db_incl. *)
+
+    Instance foo_red_f_hint a: red_db cl_B (f a) :=
+      mk_red_db _ _ (@f_equal _ _ f) a (inr cl_B).
+
+
+    Class Foo (n: nat) := mk_Foo {}.
+    Global Instance foo: Foo 3 := mk_Foo _.
+
+
+    Ltac2 evar t n :=
+      let u := open_constr:(_: ltac:(exact_no_check $t)) in
+      match Constr.Unsafe.kind u with
+      | Constr.Unsafe.Cast v0 _ v1 =>
+          Message.print (Message.of_constr v0);
+          Message.print (Message.of_constr v1);
+          Std.pose n t;
+          v0
+      | _ => Control.throw Init.Assertion_failure
+      end
+    .
+
+    Ltac2 unshelve_evar e n :=
+      let v := evar e (Some n) in
+      match Constr.Unsafe.kind v with
+      | Constr.Unsafe.Evar e _ =>
+          Control.new_goal e
+      | _ => Control.throw Init.Assertion_failure
+      end
+    .
+
+
+    Goal True.
+    Proof.
+
+      let n := Fresh.in_goal ident:(_TC_) in
+      unshelve_evar constr:(nat) n > [|exact 5].
+
+      let e := constr:(red_db cl_A a) in
+      Message.print (Message.of_constr e);
+      Message.print (Message.of_constr constr:(22));
+      let n := Fresh.in_goal ident:(_TC_) in
+      Message.print (Message.of_constr constr:(33));
+      let u := open_constr:(_) in
+      unshelve_evar e n > [|typeclasses_eauto];
+      let x := (TCSearchSuccess (Std.eval_unfold [(Std.VarRef n, Std.AllOccurrences)] (Control.hyp n))) in
+      Message.print (Message.of_constr (Control.hyp n));
+      Message.print (Message.of_constr (Std.eval_unfold [(Std.VarRef n, Std.AllOccurrences)] (Control.hyp n)))
+      .
+
+
+      let n := Fresh.in_goal ident:(_TC_) in
+      Std.pose (Some n) constr:(42);
+
+      Message.print (Message.of_constr (Std.eval_unfold [(Std.VarRef n, Std.AllOccurrences)] (Control.hyp n)));
+      Message.print (Message.of_constr ((Control.hyp n)))
+      .
+
+
+      Std.pose None (Std.eval_unfold [(Std.VarRef n, Std.AllOccurrences)] (Control.hyp n))
+      .
+
+                       (Control.hyp n)).
+
+
+      n.
+
+
+      let a :=
+
+      let e := constr:(red_db cl_A a) in
+      Message.print (Message.of_constr constr:(11));
+      Control.plus
+
+        (fun _ =>
+
+           Message.print (Message.of_constr e);
+           Message.print (Message.of_constr constr:(22));
+
+           let n := Fresh.in_goal ident:(_TC_) in
+           Message.print (Message.of_constr constr:(33));
+           let u := open_constr:(_) in
+           unshelve_evar e n > [|typeclasses_eauto];
+           let x := (TCSearchSuccess (Std.eval_unfold [(Std.VarRef n, Std.AllOccurrences)] (Control.hyp n))) in
+           Message.print (Message.of_constr (Control.hyp n));
+           Message.print (Message.of_constr (Std.eval_unfold [(Std.VarRef n, Std.AllOccurrences)] (Control.hyp n)));
+
+           Message.print (Message.of_constr u);
+           Message.print (Message.of_constr constr:(55));
+           Control.zero (TCSearchSuccess u)
+        )
+
+        (fun e =>
+           Message.print (Message.of_constr constr:(66));
+           Message.print (Message.of_exn e);
+           match e with
+           | TCSearchSuccess c =>
+               Message.print (Message.of_constr constr:(c));
+               c
+           | _ =>
+               constr:(3)
+               (* Control.zero e *)
+           end
+        ).
+
+      or
+        (fun _ =>
+           let e := constr:(red_db cl_A a) in
+           Message.print (Message.of_constr constr:(11));
+
+
+           Control.plus
+
+             (fun _ =>
+
+                Message.print (Message.of_constr constr:(22));
+
+
+                let n := Fresh.in_goal ident:(_TC_) in
+                unshelve_evar e n > [|typeclasses_eauto];
+                Control.zero (TCSearchSuccess (Std.eval_unfold [(Std.VarRef n, Std.AllOccurrences)] (Control.hyp n)))
+             )
+
+             (fun e =>
+                match e with
+                | TCSearchSuccess c =>
+                    c
+                | _ => Control.zero e
+                end
+        ))
+        (fun _ => ()) ()
+      .
+
+
+
+
+Ltac2 evar t n :=
+  let u := open_constr:(_: ltac:(exact_no_check $t)) in
+  match Constr.Unsafe.kind u with
+  | Constr.Unsafe.Cast v _ _ =>
+      Std.pose n t;
+      v
+  | _ => Control.throw Init.Assertion_failure
+  end
+.
+
+Ltac2 unshelve_evar e n :=
+  let v := evar e (Some n) in
+  match Constr.Unsafe.kind v with
+  | Constr.Unsafe.Evar e _ =>
+      Control.new_goal e
+  | _ => Control.throw Init.Assertion_failure
+  end
+.
+
+Ltac2 unshelve_evar1 e n :=
+  (ltac1:(e n|- unshelve evar (n: e))) (Ltac1.of_constr e) (Ltac1.of_ident n)
+.
+
+Ltac2 tcsearch e :=
+  Control.plus
+    (fun _ =>
+       let n := Fresh.in_goal ident:(_TC_) in
+       unshelve_evar e n > [|typeclasses_eauto];
+       Control.zero (TCSearchSuccess (Std.eval_unfold [(Std.VarRef n, Std.AllOccurrences)] (Control.hyp n)))
+    )
+    (fun e =>
+       match e with
+       | TCSearchSuccess c =>
+           c
+       | _ => Control.zero e
+       end
+    )
+.
+
+
+
+      time (do 100 (let _ := tcsearch constr:(red_db cl_A a) in ())).
+
+        let x := tcsearch constr:(red_db cl_A a) in ().
+
+      exact I.
+    Qed.
+
+
+    Goal forall (n: nat) (H: sim c ((n, f z), q) n),
+        sim a ((n, f x), p) n.
+    Proof.
+
+      Set Ltac Profiling.
+      intros n H.
+
+      ctx_rw open_constr:(fun t => _ t _ _).
+      {
+
+        let c := constr:(cl_A) in
+        match! Control.goal () with
+        | ?term = _ =>
+            Message.print (Message.of_constr constr:((@red_db $c _ $term)));
+            let tc := tcsearch constr:(@red_db $c _ $term) in
+            let lem := (Std.eval_red (Std.eval_red constr:(red_lemma $tc))) in
+            let focused := (Std.eval_red (Std.eval_red constr:(red_focused $tc))) in
+            let next := (Std.eval_red (Std.eval_red constr:(red_next $tc))) in
+            let next := match! next with
+                        | inr ?next => Some constr:($next)
+                        | inl _ => None
+                        end in
+            _red_tac next focused (fun () => eapply $lem)
+        | _ =>
+            Message.print (Message.of_int 999)
+        end.
+
+
+        let x := tcsearch constr:(red_db cl_A a) in ().
+
+
+
+
+  match c with
+  | Some c =>
+      or (fun _ =>
+            let tc := tcsearch constr:(@red_db $c _ $term) in
+            let lem := (Std.eval_red (Std.eval_red constr:(red_lemma $tc))) in
+            let focused := (Std.eval_red (Std.eval_red constr:(red_focused $tc))) in
+            let next := (Std.eval_red (Std.eval_red constr:(red_next $tc))) in
+            let next := match! next with
+                        | inr ?next => Some constr:($next)
+                        | inl _ => None
+                        end in
+            _red_tac next focused (fun _ => k; eapply $lem))
+         (fun _ =>
+            let tc := tcsearch constr:(@red_db_incl $c) in
+            let next := (Std.eval_red (Std.eval_red constr:(red_db_incl_next $tc))) in
+            _red_tac (Some next) term k)
+  | None =>
+      k ()
+  end.
+
+        let c := constr:(cl_A) in
+        match! Control.goal () with
+        | ?term = _ =>
+            (_red_tac (Some c) term (fun _ => ()))
+        | _ =>
+            Message.print (Message.of_int 999)
+        end.
+
+
+        red_tac constr:(cl_A).
+
+
+        match! goal with
+        | [ |- ?term = _ ] =>
+            (_red_tac (Some c) term (fun _ => ()))
+        end.
+
+
+        red_tac constr:(cl_A).
+
+
+      try (prw ltac:(red_tac cl_A) 3 0).
+      Show Proof.
+
+      ltac2:(open_constr:(_ : ltac:(exact_no_check $type))).
+
+      (prw ltac:(red_tac cl_C) 2 1 0).
+      (prw ltac:(red_tac cl_B) 2 2 1 0).
+      exact H.
+      Show Ltac Profile.
+    Qed.
+
+  End FOO.
+End TUTORIAL.
+
+
+
+Module TUTORIAL.
+  Section FOO.
+    (* Variables *)
+    Variable A B C: Type.
+    Variable a b c d: A.
+    Variable x y z: B.
+    Variable p q: C.
+
+    Variable sim: A -> (nat * B) * C -> nat -> Prop.
+
+    (* First Step: Prove Reduction Lemmas *)
+    Hypothesis foo_red0: a = b.
+    Hypothesis foo_red1: b = c.
+    Hypothesis foo_red2: c = d.
+    Hypothesis foo_red3: x = y.
+    Hypothesis foo_red4: y = z.
+    Hypothesis foo_red5: p = q.
+
+    (* Second Step: Define Reduction Strategy (= red_tac) *)
+    Ltac red_A f := (* f is a flag indicating continue/break/fail. Must set f before return *)
+      ((apply foo_red0; fail) ||
+       (apply foo_red1; fail) ||
+       (apply foo_red2; fail)).
+
+    (* We also give more conenient syntax *)
+    Ltac red_A' := (* = red_A *)
+      rwal (foo_red0, _continue)
+           (foo_red1, _break)
+           (foo_red2, _break)
+           0.
+
+    Ltac red_B :=
+      rwcl foo_red3
+           foo_red4
+           0.
+
+    Ltac red_B' f := (* = red_B *)
+      ((instantiate (f:=_continue); apply foo_red3; fail) ||
+       (instantiate (f:=_continue); apply foo_red4; fail)).
+
+    Ltac red_C :=
+      rrw foo_red5 _break.
+
+    Ltac red_C' := (* = red_C *)
+      rwbl
+        foo_red5
+        0.
+
+    Ltac red_C'' f := (* = red_C *)
+      (instantiate (f:=_break); apply foo_red5; fail).
+
+    (* Done. Let's use it! *)
+    Lemma foo: forall (n: nat) (H: sim c ((n, z), q) n),
+        sim a ((n, x), p) n.
+    Proof.
+      intros n H.
+      (* goal = sim a (n, x, p) n *)
+      prw red_B 2 2 1 0.
+      (* prw [red_tac (= red_r)] [l_0 (= 2)] [l_1 (= 2)] [l_2 (= 1)] ... 0 =>
+         - find the right l_0th term x0 (= ((n, x), p)) in the goal
+         - find the right l_1th term x1 (=  (n, x)    ) in the   x0
+         - find the right l_2th term x2 (=      x     ) in the   x1
+         ...
+         - the last argument must be 0. reduce the x2 following _red_r *)
+      (* goal = sim a (n, z, p) n *)
+      prw red_A 3 0.
+      (* goal = sim c (n, z, p) n *)
+      prw red_C 2 1 0.
+      (* goal = sim c (n, z, q) n *)
+      exact H.
+    Qed.
+  End FOO.
+End TUTORIAL.
+
+
+(* Ltac2 ctx_rw p := *)
+(*   let x := open_constr:(_) in *)
+(*   let c := Std.eval_hnf open_constr:($p $x) in *)
+(*   Std.unify c (Control.goal ()); *)
+(*   refine (open_constr:(@eq_rect_r _ _ $p _ $x _)) > *)
+(*     [Control.shelve ()|hnf|]. *)
+
 
 (* Ltac2 __prw red_tac success := *)
 (*   cbn; *)
@@ -55,19 +491,82 @@ Instance foo: Foo 3 := mk_Foo _.
 
 Import Constr.Binder Constr.Unsafe.
 
-
-
-From Ltac2 Require Export Ltac2.
-
 Goal forall X Y (f: X -> Y) (x: X) (y: Y), f x = y.
 Proof.
   intros.
-  let d := open_constr:(fun _t => _ _t = _) in
-  let c := Std.eval_hnf open_constr:($d _) in
+
+  Show Proof.
+
+  let p := open_constr:(fun t => _ t = _) in
+  let x := open_constr:(_) in
+  let c := Std.eval_hnf open_constr:($p $x) in
   Std.unify c (Control.goal ());
-  Std.pose None d;
-  let c := open_constr:(@eq_rect_r X _ $d) in
+  refine (open_constr:(@eq_rw _ $p $x _ _ _)) >
+    [Control.shelve ()| |hnf].
+
+
+  ctx_rw open_constr:(fun t => _ t = _).
+  Show Proof.
+
+  let p := open_constr:(fun t => _ t = _) in
+  let x := open_constr:(_) in
+  let c := Std.eval_hnf open_constr:($p $x) in
+  Std.unify c (Control.goal ());
+  refine (open_constr:(@eq_rect_r _ _ $p _ $x _)) >
+    [Control.shelve ()|hnf|].
+
+
+  (let p := open_constr:(fun t => _ t = _) in
+   let x := open_constr:(_) in
+   let c := Std.eval_hnf open_constr:($p $x) in
+   Std.unify c (Control.goal ());
+   (Control.refine (fun _ => open_constr:(@eq_rect_r _ _ $p _ $c _)))).
+
+
+  >
+    [pose 0|pose 1|pose 2|pose 3|pose 4|pose 5|pose 6].
+
+  Control.shelve().
+  Control.shelve().
+  hnf.
+
+
+  > [Control.shelve()|..])).
+
+
+  let p := open_constr:(fun t => _ t = _) in
+  let x := open_constr:(_) in
+  let c := Std.eval_hnf open_constr:($p $x) in
+  Std.unify c (Control.goal ());
+  refine (open_constr:(@eq_rect_r _ _ $p _ $x _)) >
+    [Control.shelve ()|hnf|].
+
+
+
+  (let p := open_constr:(fun t => _ t = _) in
+   let x := open_constr:(_) in
+   (Control.refine (fun _ => open_constr:(@eq_rect_r _ _ $p _ $x _)) > [Control.shelve()|..])).
+
+  Control.shelve ().
+                                                  hnf.
+
+  >
+    [Control.shelve ()|hnf|].
+
+
+  time (let p := open_constr:(fun t => _ t = _) in
+        let x := open_constr:(_) in
+        let c := Std.eval_hnf open_constr:($p $x) in
+        Std.unify c (Control.goal ());
+        refine (open_constr:(@eq_rect_r _ _ $p _ $x _)) >
+          [Control.shelve ()|hnf|]).
+
+Show Proof.
+
+  let c := open_constr:(@eq_rect_r _ _ $x _ _ _) in
   eapply $c.
+
+  Show Proof.
 
 
  _ _ _)
@@ -378,5 +877,3 @@ Module TUTORIAL.
     Qed.
   End FOO.
 End TUTORIAL.
-
-? _
